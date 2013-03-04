6c6f1cbab7d1e93bf60052334601c7f939064018
Stefan `Sec` Zehl <sec@42.org>
2013-03-04 11:46:56 +0100

    "current" version of do_tty

--- do_tty/do_tty.c
+++ do_tty/do_tty.c
@@ -1,11 +1,14 @@
 /*
  * do_tty.c - v 1.2 by Stefan `Sec` Zehl <sec@42.org> - 17.02.98 02:21
  * do_tty.c - v 1.3 by Stefan `Sec` Zehl <sec@42.org> - 20.08.98 00:46
+ * do_tty.c - v 1.4 by Stefan `Sec` Zehl <sec@42.org> - 05.12.03 00:16
  *
  * Types something on another tty via TIOCSTY
  *
  * Do whatever you like with this program, as long as you leave the copyright
  * notice intact.
+ *
+ * This code is under the 2-Clause BSD licence
  *
  */
 
@@ -19,7 +22,8 @@ int main(int argc,char **argv)
 {
 	char * device;
 	char * string;
-	int     fd;
+	char   c;
+	int    fd;
 
 	if(argc != 3){
 		printf("usage: %s <device> <string>\n",argv[0]);
@@ -29,40 +33,42 @@ int main(int argc,char **argv)
 		exit(1);
 	}
 
-	device = argv[1];
-	string=(char *)calloc(1,strlen(argv[2])+3);
-	strcpy(string,argv[2]);
-	string[100]=0;
-
-	/*
-	 * Open the device.
-	 */
+	device= argv[1];
+	string= argv[2];
 
 	if ((fd = open(device, 2)) < 0){
 		perror("open");
 		exit(1);
 	}
 
-	while(*(string)){
-		if (*string == '^'){
-			string++;
-			if(*string > 64){
-				if((*string >96)&&(*string<123))
-					*string-=32;
-				*string-=64;
-			}
-		} else if (*string == '\\'){
-			switch (*(++string)){
-				case 'a': *string='\a';break;
-				case 'b': *string='\b';break;
-				case 'f': *string='\f';break;
-				case 'n': *string='\n';break;
-				case 'r': *string='\r';break;
-				case 't': *string='\t';break;
-				case 'v': *string='\v';break;
+	while((c=*(string++))){
+		if (c == '^'){			/* Control-characters */
+			c=*(string++);
+			if(c > 64){
+				if((c >96)&&(c<123))
+					c-=32;
+				c-=64;
+			} else if (c==0){
+				fprintf(stderr,"String ends in escape\n");
+				exit(1);
+			};
+		} else if (c == '\\'){	/* Backslash-Escaped */
+			switch (c=*(string++)){
+				case 'a': c='\a';break;
+				case 'b': c='\b';break;
+				case 'f': c='\f';break;
+				case 'n': c='\n';break;
+				case 'r': c='\r';break;
+				case 't': c='\t';break;
+				case 'v': c='\v';break;
+				case '0': c=0   ;break;
+				case  0 : 
+						  fprintf(stderr,"String ends in escape\n");
+						  exit(1);
+						  break;
 			}
 		}
-		if (ioctl(fd, TIOCSTI, string++) < 0){
+		if (ioctl(fd, TIOCSTI, &c) < 0){
 			perror("TIOCSTI");
 			exit(1);
 		}
